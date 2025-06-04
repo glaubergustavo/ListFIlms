3ab979afd89d3d523c613008c0687af695519193
Glauber Gustavo <glauber.gustavo@madeinwebcombr.appleid.com>
2025-06-04 15:25:52 -0300

    Initial Commit

---
+++ ListFilms/ListFilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListFilmsApp.swift
+//  ListFilms
+//
+//  Created by Madeinweb on 04/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ListFilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
